11e46a19d874b1705069ee583d8de84288a7bb2d
Andrew Shehata <andrew.shehata@student.guc.edu.eg>
2025-06-11 16:53:44 +0300

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    historyApiFallback: true
+    historyApiFallback: true,
+    proxy: {
+    '/api': 'https://pos-webapp.railway.internal'
+  }
   }
 })
